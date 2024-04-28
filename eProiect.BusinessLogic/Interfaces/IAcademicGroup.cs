013ee7dfcf1a40b4dd3ebc6e854b5c69eed547db
bercovasile <bercovasile@gmail.com>
2024-04-28 19:48:08 +0300

    Add AcademicGroupBLL

---
+++ eProiect.BusinessLogic/Interfaces/IAcademicGroup.cs
@@ -0,0 +1,19 @@
+﻿using eProiect.Domain.Entities.Academic.DBModel;
+using eProiect.Domain.Entities.Academic;
+using eProiect.Domain.Entities.Responce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProiect.BusinessLogic.Interfaces
+{
+     public interface IAcademicGroup
+     {
+          AcademicGroupsList GetAllAcademicGroups();
+          ActionResponse AddNewAcademicGroups(AcademicGroup academicGroup);
+          ActionResponse EditAcademicGroups(AcademicGroup updateAcademicGroup);
+          ActionResponse DeleteAcademicGroups(int Id);
+     }
+}
