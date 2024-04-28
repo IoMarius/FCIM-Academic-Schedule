2c763b6a613c3fa730307c56c1c0388e0771ca02
bercovasile <bercovasile@gmail.com>
2024-04-28 19:53:45 +0300

    Add AdminControler And Admin Layout

--- eProiect/Controllers/AdminController.cs
+++ eProiect/Controllers/AdminController.cs
@@ -11,21 +11,31 @@ using eProiect.Models.ViewModels;
 using eProiect.Atributes;
 using eProiect.Domain.Entities.User;
 using eProiect.Domain.Entities.Academic;
+using eProiect.Domain.Entities.User.DBModel;
 
 namespace eProiect.Controllers
 {
     public class AdminController : BaseController
     {
 
-          private readonly ISession _session;
+          private readonly IAdmin _admin;
+          private readonly IDiscipline _discipline;
+          private readonly IAcademicGroup _academicGroup;
+          private readonly IClassRoom _classRoom;
+
+
           public AdminController()
           {
                var bl = new BusinessLogic.BuissinesLogic();
-               _session = bl.GetSessionBL();
+               _admin = bl.GetAdminBL();
+               _academicGroup = bl.GetAcademicGroupBL();
+               _classRoom = bl.GetClassRoomBL();
+               _discipline = bl.GetDisciplineBL();
           }
 
 
           [UserMode(UserRole.admin)]
+          
           public ActionResult Users()
           {
                SessionStatus();
@@ -50,7 +60,7 @@ namespace eProiect.Controllers
                };
                viewData .UData = UData;
 
-               var users = _session.GetAllUsers();
+               var users = _admin.GetAllUsers();
 
 
                foreach (var user in users.Users)
@@ -74,35 +84,56 @@ namespace eProiect.Controllers
           }
 
           [HttpPost]
+          [UserMode(UserRole.admin)]
+          
           public ActionResult SaveUser(UserEsentialData userData)
           {
-               // Verificăm dacă utilizatorul este autentificat
                SessionStatus();
                if ((string)System.Web.HttpContext.Current.Session["LoginStatus"] != "login")
                {
-                    return RedirectToAction("Login", "Login"); // Redirecționăm către pagina de login dacă utilizatorul nu este autentificat
+                    return RedirectToAction("Login", "Login");
                }
 
-               // Verificăm dacă datele primite sunt valide
                if (ModelState.IsValid)
                {
-                    // Aici poți procesa datele primite de la client
-                    // De exemplu, le poți salva în baza de date sau efectua alte operații necesare
+                    try
+                    {
+                         // Construct edited user data
+                         var editedUserData = new User
+                         {
+                              Id = userData.Id,
+                              Name = userData.Name,
+                              Surname = userData.Surname,
+                              Level = userData.Level,
+                              Credentials = new UserCredential
+                              {
+                                   Id = userData.Id,
+                                   Email = userData.Email
+                              }
+                         };
 
-                    // În exemplul de mai jos, presupunem că salvăm datele primite într-o variabilă de tip UserEsentialData
-                    var savedUserData = userData;
+                         // Call session method to edit user
+                         var response = _admin.EditUsers(editedUserData);
 
-                    // Returnăm un răspuns către client pentru a indica că datele au fost procesate cu succes
-                    return Json(new { success = true, message = "Datele au fost salvate cu succes." });
+                         // Return success or failure message
+                         return Json(new { success = response.Status, message = response.ActionStatusMsg });
+                    }
+                    catch (Exception ex)
+                    {
+                         // Handle any exceptions
+                         return Json(new { success = false, message = "An error occurred while editing user data: " + ex.Message });
+                    }
                }
                else
                {
-                    // Returnăm un răspuns de eroare către client dacă datele nu sunt valide
-                    return Json(new { success = false, message = "Eroare la validarea datelor." });
+                    // Return validation error message
+                    return Json(new { success = false, message = "Invalid data. Please check the provided information." });
                }
           }
 
           [UserMode(UserRole.admin)]
+          
+
           public ActionResult Disciplines()
           {
                SessionStatus();
@@ -123,13 +154,15 @@ namespace eProiect.Controllers
                };
                viewData.UData = UData;
 
-               viewData.DisciplinesDataList = _session.GetAllDisciplines();
+               viewData.DisciplinesDataList = _discipline.GetAllDisciplines();
 
                return View(viewData);
           }
 
 
           [UserMode(UserRole.admin)]
+         
+
           public ActionResult AcademicGroups()
           {
                SessionStatus();
@@ -150,12 +183,14 @@ namespace eProiect.Controllers
                };
                viewData.UData = UData;
 
-               viewData.AcademicGroupsDataList = _session.GetAllAcademicGroups();
+               viewData.AcademicGroupsDataList = _academicGroup.GetAllAcademicGroups();
 
                return View(viewData);
           }
 
           [UserMode(UserRole.admin)]
+          
+
           public ActionResult ClassRooms()
           {
                SessionStatus();
@@ -176,7 +211,7 @@ namespace eProiect.Controllers
                };
                viewData.UData = UData;
 
-               viewData.CassRoomsDataList = _session.GetAllCassRooms();
+               viewData.CassRoomsDataList = _classRoom.GetAllCassRooms();
 
                return View(viewData);
           }
