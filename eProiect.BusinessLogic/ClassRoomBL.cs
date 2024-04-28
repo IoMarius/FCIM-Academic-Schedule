7fdcc5a8f4db9017dff50653ec2168b07c426f42
bercovasile <bercovasile@gmail.com>
2024-04-28 19:50:38 +0300

    Add ClassRoomBLL

---
+++ eProiect.BusinessLogic/ClassRoomBL.cs
@@ -0,0 +1,34 @@
+﻿using eProiect.Domain.Entities.Academic.DBModel;
+using eProiect.Domain.Entities.Academic;
+using eProiect.Domain.Entities.Responce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eProiect.BusinessLogic.Core;
+using eProiect.BusinessLogic.Interfaces;
+
+namespace eProiect.BusinessLogic
+{
+     public class ClassRoomBL :ClassRoomApi ,IClassRoom
+     {
+          public GroupOfCassRooms GetAllCassRooms()
+          {
+               return GetAllClassRoom();
+          }
+          public ActionResponse AddNewClassRooms(ClassRoom classRoom)
+          {
+               return AddNewClassRoom(classRoom);
+          }
+          public ActionResponse EditClassRooms(ClassRoom updatedClassRoomData)
+          {
+               return EditClassRoom(updatedClassRoomData);
+          }
+          public ActionResponse DeleteClassRooms(int Id)
+          {
+               return DeleteClassRoom(Id);
+          }
+
+     }
+}
---
+++ eProiect.BusinessLogic/Interfaces/IClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using eProiect.Domain.Entities.Academic.DBModel;
+using eProiect.Domain.Entities.Academic;
+using eProiect.Domain.Entities.Responce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProiect.BusinessLogic.Interfaces
+{
+     public interface IClassRoom
+     {
+          GroupOfCassRooms GetAllCassRooms();
+          ActionResponse AddNewClassRooms(ClassRoom classRoom);
+          ActionResponse EditClassRooms(ClassRoom updateClassRoom);
+          ActionResponse DeleteClassRooms(int Id);
+
+     }
+}
