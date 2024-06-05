5bcae6b234d0a2396c3b5756ca936b5f0c1fd741
bercovasile <bercovasile@gmail.com>
2024-06-05 15:48:58 +0300

    Add_UserDiscipline_into_Navbar

---
+++ eProiect.BusinessLogic/Migrations/202406051240210_Add_UserResetTable.cs
@@ -0,0 +1,18 @@
+﻿namespace eProiect.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_UserResetTable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Students", "Email", c => c.String(nullable: false, maxLength: 30));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Students", "Email", c => c.String(nullable: false, maxLength: 90));
+        }
+    }
+}
---
+++ eProiect.BusinessLogic/Migrations/202406051243443_Add_UserResetTable_test1.cs
@@ -0,0 +1,16 @@
+﻿namespace eProiect.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_UserResetTable_test1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
